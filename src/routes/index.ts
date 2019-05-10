31fd7f05871b143eaf39b48b1d92885f8f13e4a4
zimmed <zimmed@zimmed.io>
2019-05-10 17:54:49 -0400

    Updated routes.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,6 @@
-export { default as Home } from './Home';
-export { default as About } from './About';
-export { default as Connect } from './Connect';
+import Home from './Home';
+import Connect from './Connect';
+import About from './About';
+import Info from './Info';
+
+export default [Home, Info, Connect, About];
